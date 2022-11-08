7314d1d408b8453ceb1f0ff5cd4d40f8689eb1e9
Yakobbb <jakobbachhausen@gmail.com>
2022-11-07 19:19:32 -0600

    Add all pages to nav bar

--- screens/NavBar.js
+++ screens/NavBar.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import ProfileScreen from "./Profile";
 import HomeScreen from "./Home";
+import ActivityScreen from "./Activity";
+import MessagesScreen from "./Messages";
 
 // Screen that will introduce the menu bar
 const Tab = createBottomTabNavigator();
@@ -11,6 +13,8 @@ const NavBarTabs = () => {
   return (
     <Tab.Navigator>
         <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Activity" component={ActivityScreen} />
+        <Tab.Screen name="Messages" component={MessagesScreen} />
         <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
